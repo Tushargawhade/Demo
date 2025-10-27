10d57a57287bdabdf0e7bb4057d032365197f1a7
Tusshar <tushargawhade7@gmail.com>
2025-10-27 23:45:33 +0530

    Nav created

---
+++ src/components/Nav.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function Nav() {
+  return (
+
+      <nav className="w-[15%] h-full bg-zinc-100 pt-5 flex flex-col items-center">
+        <a
+          className="py-2 px-5 border rounded-md border-blue-300 text-blue-400 "
+          href="/create"
+        >
+          Add New Product
+        </a>
+
+        <hr className="my-3 w-[80%] bg-zinc-200" />
+
+        <h1 className="text-2xl w-[80%] mb-3  ">Category Filter</h1>
+
+        <ul className=" w-[80%]">
+          <li className="mb-3 flex items-center">
+            <span className="w-[15px] h-[15px] rounded-full mr-3 bg-blue-100"></span>{" "}
+            cat 1
+          </li>
+
+          <li className="mb-3 flex items-center">
+            <span className="w-[15px] h-[15px] rounded-full mr-3 bg-red-100"></span>{" "}
+            cat 1
+          </li>
+
+          <li className="mb-3 flex items-center">
+            <span className="w-[15px] h-[15px] rounded-full mr-3 bg-green-100"></span>{" "}
+            cat 1
+          </li>
+        </ul>
+      </nav>
+    
+    
+  )
+}
+
+export default Nav
