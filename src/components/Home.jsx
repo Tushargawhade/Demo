2b0bf19954072fdd7fa1c90f5eea6992a29c5bea
Tusshar <tushargawhade7@gmail.com>
2025-10-31 22:00:18 +0530

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Nav from "./Nav";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <>
-        <Nav/>
-
+      <Nav />
 
       <div className="w-[85%] p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto">
-        <div className="card w-[18%] h-[30vh] flex flex-col justify-center items-center rounded border shadow mr-3 mb-2 p-3  ">
+        <Link
+          to="/details/1"
+          className="card w-[18%] h-[30vh] flex flex-col justify-center items-center rounded border shadow mr-3 mb-2 p-3  "
+        >
           <div
             className="w-full h-[80%] mb-3 bg-contain bg-no-repeat bg-center  hover:scale-110  "
             style={{
@@ -16,8 +19,9 @@ function Home() {
                 "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png)",
             }}
           ></div>
+
           <h1 className="hover:text-blue-400">Lorem ipsum sit amet.</h1>
-        </div>
+        </Link>
       </div>
     </>
   );
